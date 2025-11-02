bb53b96e35d9f938b9875543d725d2e1fa51e99f
digitalEnot <evgeni.novikk@icloud.com>
2025-11-02 17:30:04 +0300

    Initial Commit

---
+++ ShopOnTheCoach/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShopOnTheCoach
+//
+//  Created by Evgeni Novik on 02.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
